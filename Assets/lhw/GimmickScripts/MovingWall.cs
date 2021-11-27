1a2320cf51de3fe1903ad04ca93cb6d9205b1350
hyowon lee <woni03721@naver.com>
2021-11-27 23:59:26 +0900

    기믹 수정

--- Assets/lhw/GimmickScripts/MovingWall.cs
+++ Assets/lhw/GimmickScripts/MovingWall.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class MovingWall : MonoBehaviour
 {
-    public float upSpeed;
+    public float duration;
+    private Vector3 startPosition;
+    public float wallHeight;
 
-    public void WallUp()
+    private float startTime;
+
+    private void Awake()
     {
-        Vector3 offset = new Vector3(0, transform.localScale.y, 0);
-        transform.position = Vector3.Lerp(transform.position, offset + transform.position, upSpeed);
+        startPosition = transform.position;
+    }
+
+    public IEnumerator MoveWall()
+    {
+        startTime = Time.timeSinceLevelLoad;
+
+        while (Time.timeSinceLevelLoad < startTime+duration)
+        {
+            transform.position = Vector3.Lerp(startPosition, startPosition + Vector3.up * wallHeight, (Time.timeSinceLevelLoad - startTime)/duration);
+            yield return null;
+        }
     }
 }
