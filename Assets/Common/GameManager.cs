d4f7cfe90e79e853d6b488bae7b5ddcbe2add1b2
MyeungJunLee <audwns356@gmail.com>
2021-11-27 21:40:03 +0900

    no message

---
+++ Assets/Common/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+
+public class GameManager : MonoSingleton<GameManager>
+{
+    private bool isInit = false;
+
+    private void Start()
+    {
+    }
+  
+}
